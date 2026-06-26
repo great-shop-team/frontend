74eee5f82dcd5b930c2dcdd96a1abf2a48abb3f8
Kateryna Rostovtseva <141571972+KaterynaRostovtseva@users.noreply.github.com>
2026-06-26 21:19:12 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  /* Здесь могут быть ваши другие старые настройки (например, reactStrictMode, experimental и т.д.) */
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.synthetic.com.ua',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
